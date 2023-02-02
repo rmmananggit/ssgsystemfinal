f54dd9d5da043d162f31dd751ea348e36188c16f
Rochell Mark Mananggit <rmmananggit@gmail.com>
2023-02-02 09:47:32 +0800

    fixes

--- admin/settings.php
+++ admin/settings.php
@@ -1,6 +1,26 @@
 <?php include('authentication.php'); ?>
 <?php include('includes/header.php');?>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        </button>
+      </div>
+      <div class="modal-body"> Are you sure you want to logout?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <form action="code.php" method="POST">
+          <button type="submit" name="logout_btn" class="btn btn-danger">Logout</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <div class="container-fluid px-4 mt">
 <ol class="breadcrumb mb-4 mt-4">    
             <li class="breadcrumb-item">Account</li>
@@ -27,7 +47,7 @@
                                 {
                              ?>
             
-                    <form action="code.php" method="POST">  
+                    <form action="code.php" method="POST" enctype="multipart/form-data">  
                     <input type="hidden" name="user_id" value="<?=$user['user_id'];?>">
                     <div class="row"> 
 
@@ -73,12 +93,29 @@
                                 <input type="file" name="back" id="back" accept=".jpg, .jpeg, .png" value="">
                     </div>
 
+                    <div class="col-md-6 text-center mt-5">
+         
+                                <?php 
+                                        echo '<img class="img-fluid img-bordered-sm" src = "data:image;base64,'.base64_encode($user['front']).'" 
+                                        alt="image" style="height: 250px; max-width: 310px; object-fit: cover;">';
+                                        ?>
+                    </div>
+
+                    
+                    <div class="col-md-6  text-center mt-5">
+
+                                <?php 
+                                        echo '<img class="img-fluid img-bordered-sm" src = "data:image;base64,'.base64_encode($user['back']).'" 
+                                        alt="image" style="height: 250px; max-width: 310px; object-fit: cover;">';
+                                        ?>
+                    </div>
+
 
                      </div>   
 
-                     <div class="text-right">
+                     <div class="text-right mt-4">
                                 <a href="index.php" class="btn btn-danger">Back</a>
-                                <button type="submit" name="add_officer" class="btn btn-primary">Save</button>
+                                <button type="submit" name="update_account" class="btn btn-primary">Save</button>
                                 </div>
                                
                             </form>
@@ -102,5 +139,5 @@
 
 
 
-
+    <?php include('includes/scripts.php');?>
 <?php include('includes/footer.php');?>
